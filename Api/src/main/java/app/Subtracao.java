021fe370f3b8fd59d1d8e4fdced9f6e70aa6e340
lyvianunes <lmns3@aluno.ifal.edu.br>
2022-09-26 20:43:55 +0000

    Teste de subtração com bugs

--- Api/src/main/java/app/Subtracao.java
+++ Api/src/main/java/app/Subtracao.java
@@ -5,7 +5,7 @@ import io.jooby.annotations.*;
 @Path("/subtracao/{numUm}/{numDois}")
 public class Subtracao {
     @GET
-    public long calculaSubtracao(@PathParam int numUm, @PathParam int numDois) {
+    public long calculaSubtracao(@PathParam double numUm, @PathParam double numDois) {
         return numUm-numDois;
     }
 }
